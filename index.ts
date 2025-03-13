ffc7be5c9139cd0579494ffc26df7fe89bfda5cc
Bui Duc Hung <duchungbui1@gmail.com>
2025-03-13 22:03:45 +0700

    Create project

---
+++ index.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from "express";
+
+import dotenv from "dotenv";
+dotenv.config();
+
+import sequelize from "./config/database";
+sequelize;
+
+const app: Express = express();
+const port: number = 3000;
+
+app.set('views', `${__dirname}/views`); // Tìm đến thư mục tên là views
+app.set('view engine', 'pug'); // template engine sử dụng: pug
+
+app.get("/tours", (req: Request, res: Response) => {
+    res.render("client/pages/tours/index");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
